a0a5d0013ab92d6601742be155428ef03496cc74
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2018-06-11 11:21:12 +0200

    examples update

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -19,19 +19,17 @@ const ReactTableFixedColumns = withFixedColumns(ReactTable);
 const getData = () => {
   const data = [];
   for (let i = 0; i < 100; i += 1) {
-    const lastNameCount = mathsRandomInt(1, 3);
-    let lastName = Array.apply(null, Array(lastNameCount)); // eslint-disable-line
-    lastName = lastName.map((_, index) => ({
-      id: index,
-      value: getLastName(),
-    }));
+    // const lastNameCount = mathsRandomInt(1, 3);
+    // let lastName = Array.apply(null, Array(lastNameCount)); // eslint-disable-line
+    // lastName = lastName.map((_, index) => ({
+    //   id: index,
+    //   value: getLastName(),
+    // }));
 
     data.push({
       firstName: getFirstName(),
-      firstName2: getFirstName(),
-      lastName,
+      lastName: getLastName(),
       age: getAge(),
-      age2: getAge(),
       email: getEmail(),
       proEmail: getEmail(),
       street: getStreet(),
@@ -53,81 +51,130 @@ function Demo() {
           <a href="https://github.com/guillaumejasmin/react-table-hoc-fixed-columns">Github source</a>
         </div>
       </div>
-      <div className="table">
-        <ReactTableFixedColumns
-          data={getData()}
-          getTdProps={() => ({ style: { textAlign: 'center' } })}
-          filterable
-          columns={[
-            {
-              fixed: 'right',
-              columns: [
-                // {
-                //   Header: 'First Name 2',
-                //   accessor: 'firstName2',
-                //   width: 150,
-                // },
-                {
-                  Header: 'Age 2',
-                  accessor: 'age2',
-                  width: 150,
-                },
-              ],
-            },
-            {
-              Header: 'A',
-              columns: [
-                {
-                  Header: 'Age',
-                  accessor: 'age',
-                },
-                {
-                  Header: 'Email',
-                  accessor: 'email',
-                  width: 300,
-                },
-                {
-                  Header: 'Professional Email',
-                  accessor: 'proEmail',
-                  width: 300,
-                },
-                {
-                  Header: 'Street',
-                  accessor: 'street',
-                  width: 300,
-                },
-                {
-                  Header: 'Street bis',
-                  accessor: 'streetBis',
-                  width: 300,
-                },
-                {
-                  Header: 'City',
-                  accessor: 'city',
-                },
-              ],
-            },
-            {
-              fixed: true,
-              columns: [
-                {
-                  Header: 'First Name',
-                  accessor: 'firstName',
-                  width: 150,
-                  // fixed: true,
-                },
-                {
-                  Header: 'Last Name',
-                  accessor: 'lastName',
-                  width: 150,
-                  Cell: row => <div>{row.value.map(item => <div key={item.id}>{item.value}</div>)}</div>,
-                  // fixed: true,
-                },
-              ],
-            },
-          ]}
-          defaultPageSize={50}
-        />
+      <div>
+
+        <div className="table">
+          <ReactTableFixedColumns
+            data={getData()}
+            getTdProps={() => ({ style: { textAlign: 'center' } })}
+            filterable
+            columns={[
+              {
+                Header: 'First Name',
+                accessor: 'firstName',
+                width: 150,
+                fixed: 'left',
+              },
+              {
+                Header: 'Last Name',
+                accessor: 'lastName',
+                width: 150,
+                fixed: 'left',
+              },
+              {
+                Header: 'Age',
+                accessor: 'age',
+                width: 150,
+                fixed: 'right',
+              },
+              {
+                Header: 'Email',
+                accessor: 'email',
+                width: 300,
+              },
+              {
+                Header: 'Professional Email',
+                accessor: 'proEmail',
+                width: 300,
+              },
+              {
+                Header: 'Street',
+                accessor: 'street',
+                width: 300,
+              },
+              {
+                Header: 'Street bis',
+                accessor: 'streetBis',
+                width: 300,
+              },
+              {
+                Header: 'City',
+                accessor: 'city',
+              },
+            ]}
+            defaultPageSize={50}
+          />
+        </div>
+
+        <div className="table">
+          <ReactTableFixedColumns
+            data={getData()}
+            getTdProps={() => ({ style: { textAlign: 'center' } })}
+            filterable
+            columns={[
+              {
+                fixed: 'left',
+                columns: [
+                  {
+                    Header: 'First Name',
+                    accessor: 'firstName',
+                    width: 150,
+                  },
+                  {
+                    Header: 'Last Name',
+                    accessor: 'lastName',
+                    width: 150,
+                  },
+                ],
+              },
+              {
+                Header: 'Other Infos',
+                columns: [
+                  {
+                    Header: 'Full name',
+                    accessor: 'lastName',
+                    width: 150,
+                    Cell: row => <div>{row.original.firstName}<br />{row.original.lastName}</div>,
+                  },
+                  {
+                    Header: 'Age',
+                    accessor: 'age',
+                  },
+                  {
+                    Header: 'Email',
+                    accessor: 'email',
+                    width: 300,
+                  },
+                  {
+                    Header: 'Professional Email',
+                    accessor: 'proEmail',
+                    width: 300,
+                  },
+                ],
+              },
+              {
+                Header: 'Location',
+                columns: [
+                  {
+                    Header: 'Street',
+                    accessor: 'street',
+                    width: 300,
+                  },
+                  {
+                    Header: 'Street bis',
+                    accessor: 'streetBis',
+                    width: 300,
+                  },
+                  {
+                    Header: 'City',
+                    accessor: 'city',
+                  },
+                ],
+              },
+            ]}
+            defaultPageSize={50}
+          />
+        </div>
       </div>
     </div>
   );
