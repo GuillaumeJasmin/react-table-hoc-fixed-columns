1d4b6f3cda630c933739a6133305a6d5d789bda6
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2018-08-15 19:23:15 +0200

    docs demo

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -121,21 +121,6 @@ function Demo() {
                     accessor: 'firstName',
                     width: 150,
                   },
-                  // {
-                  //   Header: 'Last Name',
-                  //   accessor: 'lastName',
-                  //   width: 150,
-                  // },
-                ],
-              },
-              {
-                fixed: 'left',
-                columns: [
-                  // {
-                  //   Header: 'First Name',
-                  //   accessor: 'firstName',
-                  //   width: 150,
-                  // },
                   {
                     Header: 'Last Name',
                     accessor: 'lastName',
